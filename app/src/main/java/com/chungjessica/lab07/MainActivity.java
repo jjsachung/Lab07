f68306f80196c1820f6136f13092623fafa3782a
jssachung <jessica.s.chung101@gmail.com>
2021-10-22 14:19:02 -0400

    need to animate

--- app/src/main/java/com/chungjessica/lab07/MainActivity.java
+++ app/src/main/java/com/chungjessica/lab07/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +18,26 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         //add contents to container
         ft.add(R.id.fragment, FragmentB.newInstance(99, "RulerOfTheWorld"), "FragmentB");
+        //ft.hide(getSupportFragmentManager().findFragmentByTag("Hello"));
         //complete the changes added above
         ft.commit();
+
     }
 
     public void update(View view) {
         FragmentB fragmentB = (FragmentB)getSupportFragmentManager().findFragmentByTag("FragmentB");
+        FragmentB greeting = (FragmentB)getSupportFragmentManager().findFragmentByTag("Hello");
         ((TextView)fragmentB.view.findViewById(R.id.b_textview)).setText(R.string.rockstar);
-
+        FragmentTransaction test = getSupportFragmentManager().beginTransaction();
+        if(fragmentB.isAdded())
+            test.show(fragmentB);
+        if(greeting.isAdded()) {
+            test.hide(fragmentB);
+        }
+        if(fragmentB.isHidden()) {
+            test.remove(greeting);
+//            test.show(fragmentB);
+        }
+        test.commit();
     }
 }
